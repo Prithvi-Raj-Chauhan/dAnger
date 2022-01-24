e161107b5cf13074dee6e77cae23e7b099803238
Divyanshu Padhy <divyadollypadhyoutlook.com>
2022-01-24 19:24:47 +0530

    Messaging logic and db management

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import render_template, request, Flask
+import json
+
+DATABASE = json.load(open('db.json'))
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def dAnger():
-    return render_template('index.html')
+    if request.method == 'POST':
+        msg = request.form['msg']
+        DATABASE.append(msg)
+        json.dump(DATABASE, open('db.json', "w"))
+    return render_template('index.html', db=DATABASE)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
