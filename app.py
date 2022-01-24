0f3aec950566ba164475c80e480a826d58e568ef
Divyanshu Padhy <divyadollypadhyoutlook.com>
2022-01-24 19:04:31 +0530

    Server Setup

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import render_template, request, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def dAnger():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
